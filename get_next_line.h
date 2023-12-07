1b0ae281b164eeb6f9e2a3c29db1c5f97d8a1fc9
tomjoy75 <tomjoy75@gmail.com>
2023-12-07 23:46:15 +0100

    push from home

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tjoyeux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:55:20 by tjoyeux           #+#    #+#             */
-/*   Updated: 2023/12/07 16:58:38 by tjoyeux          ###   ########.fr       */
+/*   Updated: 2023/12/07 23:29:01 by tjoyeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
+
+char	*get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,25 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tjoyeux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/07 16:59:18 by tjoyeux           #+#    #+#             */
-/*   Updated: 2023/12/07 23:28:23 by tjoyeux          ###   ########.fr       */
+/*   Created: 2023/12/07 23:29:59 by tjoyeux           #+#    #+#             */
+/*   Updated: 2023/12/07 23:32:05 by tjoyeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	int	bytes_read;
-	char	*cup_buffer;
 
-	cup_buffer = calloc(3 + 1 ,sizeof(char));
-	if (cup_buffer == NULL)
-		return (NULL);
-	bytes_read = read(fd, cup_buffer, 3);
-	return (cup_buffer);
-}
